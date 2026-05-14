e4c8413cd0cc026e94ff98ccf746ac5be8f353b0
ArthurRomao02 <arthur.romao@etec.sp.gov.br>
2026-05-14 13:44:55 -0300

    Efetuação do login

---
+++ js/script.js
@@ -0,0 +1,33 @@
+alert("Bem-vindo ao site!")
+
+let nome = prompt("Digite seu nome")
+
+console.log(nome)
+
+const temaBtn = document.getElementById("temaBtn")
+
+temaBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+})
+
+const imagens = [
+    "./img/img1.jpg",
+    "./img/img2.jpg",
+    "./img/img3.jpg"
+]
+
+let index = 0
+
+const slide = document.getElementById("slide")
+
+setInterval(() => {
+
+    index++
+
+    if(index >= imagens.length){
+        index = 0
+    }
+
+    slide.src = imagens[index]
+
+}, 3000)
